da592f29c291cbb1217ca6bd5835e79eb6aee671
Poojani Wijenayake <poojaniwijenayake@gmail.com>
2026-04-14 15:14:07 +0530

    updated

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -3,7 +3,7 @@ export default function Greetings() {
     <div className="ny-greetings">
       <div className="ny-sinhala">සුභ අලුත් අවුරුද්දක් වේවා!</div>
       <div className="ny-tamil">இனிய புத்தாண்டு நல்வாழ்த்துக்கள்!</div>
-      <div className="ny-eng">Sinhala &amp; Tamil New Year</div>
+      <div className="ny-eng">Happy Sinhala &amp; Tamil New Year</div>
     </div>
   );
 }
